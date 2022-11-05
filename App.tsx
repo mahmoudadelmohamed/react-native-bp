45b32f8b85bec1bf86df8626768b7ae44398a750
Mahmoud Adel <mahmoud.adel@koinz.app>
2022-11-05 03:22:52 +0200

    #0 - Initial Commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+
+
+
+  return (
+    <SafeAreaView>
+      <Text>Hello, Wprld</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
